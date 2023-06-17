93a3b3832cb557e74fcc2496ff8ee3e5b343d9a0
imamalif1616 <imamalif1616@gmail.com>
2023-06-17 21:32:19 +0700

    try to add new page

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, redirect, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['tki']
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/retrieve')
+def retrieve():
+    collection = db['test']
+
+    documents = collection.find()
+
+    return render_template('retrieve.html', documents=documents)
 
 @app.route('/admin')
 def admin():
